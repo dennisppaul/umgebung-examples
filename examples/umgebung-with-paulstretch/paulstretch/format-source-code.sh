f1686caea30b4e522f276528be96eab3761eb02f
dennisppaul <dennis.paul@hfk-bremen.de>
2024-05-29 17:24:34 +0200

    removed submodule

--- examples/umgebung-with-paulstretch/paulstretch/format-source-code.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-
-echo "+++ formating the following files:"
-echo 
-find . -name '*.h' -o -name '*.c' -o -name '*.cpp'
-echo
-
-find . -name '*.h' -o -name '*.c' -o -name '*.cpp'  | xargs clang-format -i
