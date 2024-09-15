bed59ed96629f445132c81a0a711b001186488e6
dennisppaul <dennis.paul@hfk-bremen.de>
2024-09-15 20:16:52 +0200

    added 2nd camera to example

--- examples/umgebung-capture/umgebung-capture.cpp
+++ examples/umgebung-capture/umgebung-capture.cpp
@@ -5,7 +5,8 @@ using namespace umgebung;
 
 class UmgebungApp : public PApplet, public CaptureListener {
 
-    Capture* capture;
+    Capture* capture_facetime;
+    Capture* capture_iphone;
 
     void settings() {
         size(1280, 720);
@@ -32,30 +33,40 @@ class UmgebungApp : public PApplet, public CaptureListener {
             std::cerr << "No capture devices found." << std::endl;
         }
 
-        capture = new Capture();
-        capture->init("FaceTime HD Camera",
-                      "1280x720",
-                      "30",
-                      "nv12");
-        capture->set_listener(this);
-        capture->start();
+        capture_facetime = new Capture();
+        capture_facetime->init("FaceTime HD Camera",
+                               "1280x720",
+                               "30",
+                               "nv12");
+        capture_facetime->set_listener(this);
+        capture_facetime->start();
+
+        capture_iphone = new Capture();
+        capture_iphone->init("d3Phone Camera",
+                             "1280x720",
+                             "30",
+                             "nv12");
+        capture_iphone->set_listener(this);
+        capture_iphone->start();
     }
 
     void draw() {
         background(0.2);
 
         fill(1);
-        if (capture != nullptr) {
-            capture->reload(); // TODO if run from thread ( i.e `myMovie->play();` ) this needs to be called in draw
-            image(capture, 0, 0, width, height);
-            image(capture,
+        if (capture_facetime != nullptr && capture_iphone != nullptr) {
+            // TODO if run from thread ( i.e `myMovie->play();` ) this needs to be called in draw
+            capture_facetime->reload();
+            capture_iphone->reload();
+            image(capture_facetime, 0, 0, width, height);
+            image(capture_iphone,
                   mouseX, mouseY,
-                  capture->width / 4, capture->height / 4);
+                  capture_iphone->width / 4, capture_iphone->height / 4);
         }
     }
 
     void captureEvent(Capture* capture) {
-        // WIP
+        // WIP TODO updating texture in non-OpenGL thread does not work
     }
 
     void keyPressed() {
