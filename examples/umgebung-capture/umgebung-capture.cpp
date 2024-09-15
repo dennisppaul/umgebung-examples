d0a146c81e55f91d9b216110a42f100ef9fc8f3a
dennisppaul <dennis.paul@hfk-bremen.de>
2024-09-15 19:51:51 +0200

    added capture listener ( WIP, does not work yet )

--- examples/umgebung-capture/umgebung-capture.cpp
+++ examples/umgebung-capture/umgebung-capture.cpp
@@ -3,7 +3,7 @@
 
 using namespace umgebung;
 
-class UmgebungApp : public PApplet {
+class UmgebungApp : public PApplet, public CaptureListener {
 
     Capture* capture;
 
@@ -12,34 +12,33 @@ class UmgebungApp : public PApplet {
     }
 
     void setup() {
-        // const std::vector<DeviceCapability> capabilities = getDeviceCapabilities();
-        // for (const auto& capability: capabilities) {
-        //     std::cout
-        //         << capability.deviceName << " "
-        //         << " resolution: " << capability.width << "x" << capability.height
-        //         << " frame rate: " << capability.minFrameRate << "-" << capability.maxFrameRate
-        //         << std::endl;
-        // }
-
-        std::vector<std::string> devices;
-        if (Capture::print_available_devices(devices)) {
+        const std::vector<std::string> devices = Capture::list();
+        if (!devices.empty()) {
             uint32_t i = 0;
             for (const auto& device: devices) {
-                std::cout << "[" << i << "]"
-                          << " " << device << std::endl;
+                std::cout
+                    << "[" << i << "]"
+                    << " " << device
+                    << std::endl;
                 i++;
+            }
+            for (const auto& device: devices) {
+                std::cout
+                    << "Capabilities for " << device
+                    << std::endl;
+                Capture::list_capabilities(device);
             }
         } else {
             std::cerr << "No capture devices found." << std::endl;
         }
 
-        Capture::list_capabilities("FaceTime HD Camera");
-
-        capture = new Capture("FaceTime HD Camera",
-                              "1280x720",
-                              "30",
-                              "0rgb");
-        capture->play();
+        capture = new Capture();
+        capture->init("FaceTime HD Camera",
+                      "1280x720",
+                      "30",
+                      "nv12");
+        capture->set_listener(this);
+        capture->start();
     }
 
     void draw() {
@@ -49,9 +48,14 @@ class UmgebungApp : public PApplet {
         if (capture != nullptr) {
             capture->reload(); // TODO if run from thread ( i.e `myMovie->play();` ) this needs to be called in draw
             image(capture, 0, 0, width, height);
-            image(capture, mouseX, mouseY,
+            image(capture,
+                  mouseX, mouseY,
                   capture->width / 4, capture->height / 4);
         }
+    }
+
+    void captureEvent(Capture* capture) {
+        // WIP
     }
 
     void keyPressed() {
