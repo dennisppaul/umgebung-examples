04c7a2ffa9e5ffe3ad26519ab0b7207879bdf171
dennisppaul <dennis.paul@hfk-bremen.de>
2025-02-21 21:50:48 +0100

    WIP

--- examples/umgebung-image/umgebung-image.cpp
+++ examples/umgebung-image/umgebung-image.cpp
@@ -2,57 +2,50 @@
 
 using namespace umgebung;
 
-class UmgebungApp : public PApplet {
+PImage* mImage;
 
-    PImage* mImage;
+void settings() {
+    size(1024, 768);
+}
 
-    void settings() {
-        size(1024, 768);
+void setup() {
+    if (!exists(sketchPath() + "../image.png")) {
+        println("cannot find required files at:", sketchPath());
+        println("... exiting");
+        exit();
     }
+    mImage = loadImage(sketchPath() + "../image.png");
 
-    void setup() {
-        if (!exists(sketchPath() + "../image.png")) {
-            println("cannot find required files at:", sketchPath());
-            println("... exiting");
-            exit();
-        }
-        mImage = loadImage(sketchPath() + "../image.png");
+    //         uint32_t pixels[64 * 64];
+    //         for (int i = 0; i < 64 * 64; ++i) {
+    //             pixels[i] = color(random(1), random(1), random(1), 0.5f);
+    //         }
+    // mImage->update(pixels, 64, 64, 32, 32);
+}
 
-//         uint32_t pixels[64 * 64];
-//         for (int i = 0; i < 64 * 64; ++i) {
-//             pixels[i] = color(random(1), random(1), random(1), 0.5f);
-//         }
-        // mImage->update(pixels, 64, 64, 32, 32);
+void draw() {
+    background(0.2f);
+
+    /* image */
+    // constexpr int length = 64 * 64 * 4; // number of channels is always 4
+    // float         pixels[length];
+    // for (int i = 0; i < length; i += 4) {
+    //     const float c = (i + frameCount) % 252 / 255.0;
+    //     pixels[i + 0] = c;
+    //     pixels[i + 1] = 1.0 - c;
+    //     pixels[i + 2] = 1.0;
+    //     pixels[i + 3] = 0.5;
+    // }
+    // mImage->update(pixels, 64, 64, 32, 32);
+
+    fill(1.0f);
+    image(mImage, mouseX, mouseY);
+    // image(mImage, padding, padding + spacing, grid, grid);
+}
+
+void keyPressed() {
+    if (key == 'q') {
+        exit();
     }
-
-    void draw() {
-        background(0.2f);
-
-        /* image */
-        // constexpr int length = 64 * 64 * 4; // number of channels is always 4
-        // float         pixels[length];
-        // for (int i = 0; i < length; i += 4) {
-        //     const float c = (i + frameCount) % 252 / 255.0;
-        //     pixels[i + 0] = c;
-        //     pixels[i + 1] = 1.0 - c;
-        //     pixels[i + 2] = 1.0;
-        //     pixels[i + 3] = 0.5;
-        // }
-        // mImage->update(pixels, 64, 64, 32, 32);
-
-        fill(1.0f);
-        image(mImage, mouseX, mouseY);
-        // image(mImage, padding, padding + spacing, grid, grid);
-    }
-
-    void keyPressed() {
-        if (key == 'q') {
-            exit();
-        }
-        println((char) key, " pressed");
-    }
-};
-
-PApplet* umgebung::instance() {
-    return new UmgebungApp();
+    println((char) key, " pressed");
 }
--- examples/umgebung-simple/umgebung-simple.cpp
+++ examples/umgebung-simple/umgebung-simple.cpp
@@ -1,76 +1,101 @@
 #include "Umgebung.h"
+#include "PGraphics.h"
 
 using namespace umgebung;
 
-class UmgebungApp : public PApplet {
+void arguments(const std::vector<std::string>& args) {
+    if (args.size() == 2) {
+        println("arguments: ", args[0], ", ", args[1]);
+    }
+    // for (std::string& s: args) {
+    //     println("> ", s);
+    //     if (begins_with(s, "--width")) {
+    //         mWidth = get_int_from_argument(s);
+    //     }
+    //     if (begins_with(s, "--height")) {
+    //         mHeight = get_int_from_argument(s);
+    //     }
+    // }
+}
 
-    PShape mShape;
-    int    mWidth  = 1024;
-    int    mHeight = 768;
+Subsystem* subsystem;
 
-    void arguments(std::vector<std::string> args) override {
-        for (std::string& s: args) {
-            println("> ", s);
-            if (begins_with(s, "--width")) {
-                mWidth = get_int_from_argument(s);
-            }
-            if (begins_with(s, "--height")) {
-                mHeight = get_int_from_argument(s);
-            }
+void add_generic_subsystem() {
+    subsystem       = new Subsystem();
+    subsystem->init = []() -> bool {
+        console("subsystem: init");
+        return true;
+    };
+    subsystem->setup_pre = []() {
+        console("subsystem: setup_pre");
+    };
+    subsystem->shutdown = []() {
+        console("subsystem: shutdown");
+    };
+    subsystem->event = [](SDL_Event* event) {
+        if (event->type == SDL_EVENT_KEY_DOWN) {
+            console("subsystem: ", (char) event->key.key, " pressed");
         }
+    };
+    subsystems.push_back(subsystem);
+}
+
+void settings() {
+    size(1024, 768);
+    // audio_input_device  = DEFAULT_AUDIO_DEVICE;
+    // audio_output_device = DEFAULT_AUDIO_DEVICE;
+    // audio_input_channels  = DEFAULT_NUMBER_OF_INPUT_CHANNELS;
+    // audio_output_channels = DEFAULT_NUMBER_OF_OUTPUT_CHANNELS;
+    display = 0;
+    // fullscreen            = true;
+    // borderless            = true;
+    // antialiasing          = 8;
+    // resizable             = true;
+    // always_on_top         = true;
+    // retina_support = true;
+    // headless              = false;
+    // no_audio              = false;
+}
+
+void setup() {
+    println("width      : ", width);
+    println("height     : ", height);
+    println("pixelWidth : ", pixelWidth);
+    println("pixelHeight: ", pixelHeight);
+    add_generic_subsystem();
+}
+
+void draw() {
+    background(1);
+
+    stroke(0.0f);
+    noFill();
+    rect(10, 10, width / 2 - 20, height / 2 - 20);
+
+    noStroke();
+    fill(random(0, 0.2));
+    rect(20, 20, width / 2 - 40, height / 2 - random(20, 40));
+}
+
+// void audioblock(float** input, float** output, int length) {
+//     for (int i = 0; i < length; i++) {
+//         float sample = random(-0.1, 0.1);
+//         for (int j = 0; j < audio_output_channels; ++j) {
+//             output[j][i] = sample;
+//         }
+//     }
+// }
+
+void keyPressed() {
+    if (key == 'q') {
+        exit();
     }
+    println((char) key, " pressed");
+}
 
-    void settings() override {
-        size(mWidth, mHeight);
-        audio_input_device    = DEFAULT_AUDIO_DEVICE;
-        audio_output_device   = DEFAULT_AUDIO_DEVICE;
-        audio_input_channels  = DEFAULT_NUMBER_OF_INPUT_CHANNELS;
-        audio_output_channels = DEFAULT_NUMBER_OF_OUTPUT_CHANNELS;
-        monitor               = 0;
-        fullscreen            = false;
-        borderless            = true;
-        antialiasing          = 8;
-        resizable             = false;
-        always_on_top         = true;
-        enable_retina_support = true;
-        headless              = false;
-        no_audio              = false;
-    }
+void mouseMoved() {}
 
-    void setup() override {
-        println("width : ", width);
-        println("height: ", height);
-    }
-
-    void draw() override {
-        background(1.0f);
-
-        stroke(0.0f);
-        noFill();
-        rect(10, 10, width / 2 - 20, height / 2 - 20);
-
-        noStroke();
-        fill(random(0, 0.2));
-        rect(20, 20, width / 2 - 40, height / 2 - 40);
-    }
-
-    void audioblock(float** input, float** output, int length) override {
-        for (int i = 0; i < length; i++) {
-            float sample = random(-0.1, 0.1);
-            for (int j = 0; j < audio_output_channels; ++j) {
-                output[j][i] = sample;
-            }
-        }
-    }
-
-    void keyPressed() override {
-        if (key == 'q') {
-            exit();
-        }
-        println((char) key, " pressed");
-    }
-};
-
-PApplet* umgebung::instance() {
-    return new UmgebungApp();
+void shutdown() {
+    delete subsystem;
+    println("finishing");
 }
