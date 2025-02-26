e274c831239000dc5a2b16fb0eb0c7a89418ff89
dennisppaul <dennis.paul@hfk-bremen.de>
2025-02-26 21:40:49 +0100

    reworked some examples to new format

--- examples/umgebung-movie/umgebung-movie.cpp
+++ examples/umgebung-movie/umgebung-movie.cpp
@@ -3,43 +3,8 @@
 
 using namespace umgebung;
 
-class UmgebungExampleAppWithMovie : public PApplet, MovieListener {
 
-    Movie* movie;
-
-    void settings() override {
-        size(1024, 768);
-    }
-
-    void setup() override {
-        movie = new Movie("../video.mp4");
-        movie->play();
-        movie->loop();
-        movie->set_listener(this);
-    }
-
-    void draw() override {
-        background(0.2f);
-        // if (myMovie->available()) {
-        //     myMovie->read();
-        // }
-        fill(1.0f);
-        movie->reload(); // TODO if run from thread ( i.e `myMovie->play();` ) this needs to be called in draw
-        image(movie, mouseX, mouseY);
-    }
-
-    void keyPressed() override {
-        if (key == 'q') {
-            exit();
-        }
-        if (key == 'p') {
-            movie->play();
-        }
-        if (key == 's') {
-            movie->pause();
-        }
-    }
-
+class MyMovieListener final : public MovieListener {
     void movieVideoEvent(Movie* m, float* audio_buffer, int length, int channels) override {}
 
     void movieAudioEvent(Movie* m, float* audio_buffer, int length, int channels) override {
@@ -48,6 +13,38 @@ class UmgebungExampleAppWithMovie : public PApplet, MovieListener {
     }
 };
 
-PApplet* umgebung::instance() {
-    return new UmgebungExampleAppWithMovie();
+Movie*          movie;
+MyMovieListener movie_listener;
+
+void settings() {
+    size(1024, 768);
+}
+
+void setup() {
+    movie = new Movie("../video.mp4");
+    movie->play();
+    movie->loop();
+    movie->set_listener(&movie_listener);
+}
+
+void draw() {
+    background(0.2f);
+    // if (myMovie->available()) {
+    //     myMovie->read();
+    // }
+    fill(1.0f);
+    movie->reload(); // TODO if run from thread ( i.e `myMovie->play();` ) this needs to be called in draw
+    image(movie, mouseX, mouseY);
+}
+
+void keyPressed() {
+    if (key == 'q') {
+        exit();
+    }
+    if (key == 'p') {
+        movie->play();
+    }
+    if (key == 's') {
+        movie->pause();
+    }
 }
--- examples/umgebung-simple/umgebung-simple.cpp
+++ examples/umgebung-simple/umgebung-simple.cpp
@@ -1,7 +1,9 @@
 #include "Umgebung.h"
-#include "PGraphics.h"
 
 using namespace umgebung;
+
+PImage* mImage;
+PFont*  mFont;
 
 void arguments(const std::vector<std::string>& args) {
     if (args.size() == 2) {
@@ -34,7 +36,7 @@ void add_generic_subsystem() {
     };
     subsystem->event = [](SDL_Event* event) {
         if (event->type == SDL_EVENT_KEY_DOWN) {
-            console("subsystem: ", (char) event->key.key, " pressed");
+            console("subsystem: '", static_cast<char>(event->key.key), "' pressed");
         }
     };
     subsystems.push_back(subsystem);
@@ -49,12 +51,13 @@ void settings() {
     display = 0;
     // fullscreen            = true;
     // borderless            = true;
-    // antialiasing          = 8;
+    antialiasing = 8;
     // resizable             = true;
     // always_on_top         = true;
     // retina_support = true;
     // headless              = false;
     // no_audio              = false;
+    subsystem_graphics = umgebung_subsystem_graphics_create_openglv33();
 }
 
 void setup() {
@@ -63,18 +66,79 @@ void setup() {
     println("pixelWidth : ", pixelWidth);
     println("pixelHeight: ", pixelHeight);
     add_generic_subsystem();
+
+    mImage = loadImage(sketchPath() + "../image.png");
+    mFont  = loadFont(sketchPath() + "../JetBrainsMono-Regular.ttf", 16);
+    textFont(mFont);
 }
 
 void draw() {
-    background(1);
+    background(1); // TODO would this flush everything?!?
+
+    /* rects */
 
     stroke(0.0f);
     noFill();
-    rect(10, 10, width / 2 - 20, height / 2 - 20);
+    rect(10, 10, 40, 40);
 
     noStroke();
-    fill(random(0, 0.2));
-    rect(20, 20, width / 2 - 40, height / 2 - random(20, 40));
+    fill(0);
+    rect(60, 10, 40, 40);
+
+    /* circle ( + ellipse ) */
+
+    fill(1, 0, 0);
+    circle(mouseX, mouseY, 40);
+
+    /* lines + bezier */
+
+    noFill();
+
+    stroke(0);
+    line(width, 0, 0, height);
+    line(0, 0, width, height);
+
+    stroke(1, 0, 0);
+    bezier(0, height, mouseX, mouseY, mouseX, mouseY, width, 0);
+    bezier(width, height, mouseX, mouseY, mouseX, mouseY, 0, 0);
+
+    /* points + pointSize */
+
+    pointSize(2);
+    stroke(0);
+    for (int i = 0; i < 100; ++i) {
+        const float x = random(110, 150);
+        const float y = random(10, 50);
+        point(x, y);
+    }
+    pointSize(1);
+
+    /* shape + transforms */
+
+    noStroke(); // TODO stroke shape do not work ATM
+    fill(1, 0, 0);
+    pushMatrix();
+    translate(180, 30);
+    rotate(PI / 4);
+    const float s = 20 / sqrtf(20 * 20 + 20 * 20);
+    scale(s);
+    beginShape(POLYGON);
+    vertex(-20, -20);
+    vertex(20, -20);
+    vertex(20, 20);
+    vertex(-20, 20);
+    endShape();
+    popMatrix();
+
+    /* image */
+
+    fill(1);
+    image(mImage, 210, 10, 80, 80);
+
+    /* text + font */
+
+    fill(0);
+    text(to_string("(", static_cast<int>(mouseX), ",", static_cast<int>(mouseY), ")").c_str(), 300, 10 + 16);
 }
 
 // void audioblock(float** input, float** output, int length) {
