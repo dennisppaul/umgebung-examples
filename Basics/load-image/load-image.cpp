139ad4c70bd1b61003a838924df445e5d13bb701
dennisppaul <dennis.paul@hfk-bremen.de>
2025-06-16 12:34:25 +0200

    updated example

--- Basics/load-image/load-image.cpp
+++ Basics/load-image/load-image.cpp
@@ -11,15 +11,26 @@ void settings() {
 
 void setup() {
     umfeld_image = loadImage("../umfeld.png");
+
+    // NOTE loading images also works with URLs
+    //      umfeld_image = loadImage("https://raw.githubusercontent.com/dennisppaul/umfeld-examples/52730a5113341dadb60046a7908bafa874ce8ab4/Basics/load-image/umfeld.png");
+
     rectMode(CENTER);
     noStroke();
+
+    // const std::string msg = loadString("data:,Hello%20world");
+    // std::string msg = loadString("data:text/html,%3Ch1%3EHello%20World%3C%2Fh1%3E");
+    // const std::string msg = loadString("data:text/plain;base64,SGVsbG8gd29ybGQ=");
+    // const std::string msg = loadString("../CMakeLists.txt");
+    // const std::string msg = loadString("https://raw.githubusercontent.com/dennisppaul/umfeld/refs/heads/main/README.md");
+    // console(msg);
 }
 
 void draw() {
     background(0.85f);
 
     fill(0.0f);
-    g->debug_text("FPS          : " + nf(frameRate, 3, 1), 10, 10);
+    g->debug_text("FPS: " + nf(frameRate, 3, 1), 10, 10);
 
     fill(0.0f);
     circle(width * 0.5f, height * 0.5f, umfeld_image->width);
