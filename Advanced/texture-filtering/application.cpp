900dfbca789de0962bcfcfafe1ded20e9de37dcf
dennisppaul <dennis.paul@hfk-bremen.de>
2025-07-11 08:51:54 +0200

    added wrapping

--- Advanced/texture-filtering/application.cpp
+++ Advanced/texture-filtering/application.cpp
@@ -24,9 +24,22 @@ void draw() {
     fill(1.0f);
     image(umfeld_image, mouseX - 256, mouseY, 512, 512);
 
-    texture(umfeld_image);   // set texture to the loaded image
-    texture_filter(NEAREST); // set to NEAREST for pixelated effect
-    rect(mouseX + 256, mouseY, 512, 512);
-    texture_filter(LINEAR); // reset to LINEAR for smooth texture filtering
-    texture();              // reset texture to default
+    texture(umfeld_image);                // set texture to the loaded image
+    texture_filter(NEAREST);              // set to NEAREST for pixelated effect
+    rect(mouseX + 256, mouseY, 512, 512); // draw rectangle with texture
+    texture_filter(LINEAR);               // reset to LINEAR for smooth texture filtering
+    texture();                            // reset texture to default
 }
+
+// NOTE to test wrapping modes ( e.g REPEAT, CLAMP_TO_EDGE, ... ) the texture coordinates need to be manipulated e.g:
+// ```
+// stroke(1.0f, 1.0f, 1.0f, 0.0f); // use `stroke` to set the border color
+// texture_wrap(CLAMP_TO_BORDER); // set texture wrap to clamp to border
+// constexpr float texture_scale = 4.0f;
+// beginShape(QUADS);
+// vertex(mouseX, mouseY - 256, 0, 0, 0);
+// vertex(mouseX + 512, mouseY - 256, 0, texture_scale, 0);
+// vertex(mouseX + 512, mouseY + 256, 0, texture_scale, texture_scale);
+// vertex(mouseX, mouseY + 256, 0, 0, texture_scale);
+// endShape();
+// ```
